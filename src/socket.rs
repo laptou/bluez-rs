c947f01333581a934a8f34e6f4d5bac419e29014
Ibiyemi Abiodun <ibiyemi@intulon.com>
2020-11-27 21:06:03 -0500

    switch to using smol for library

--- src/socket.rs
+++ src/socket.rs
@@ -1,9 +1,8 @@
 use std::os::raw::c_ushort;
 use std::u16;
 
-use async_std::io::{self};
-use async_std::os::unix::io::{FromRawFd, RawFd};
-use async_std::os::unix::net::UnixStream;
+use std::os::unix::io::{FromRawFd, RawFd};
+use smol::net::unix::UnixStream;
 use bytes::*;
 use futures::io::{AsyncReadExt, AsyncWriteExt, BufReader, ReadHalf, WriteHalf};
 use libc;
@@ -48,7 +47,7 @@ pub struct ManagementSocket {
 }
 
 impl ManagementSocket {
-    pub fn open() -> Result<Self, io::Error> {
+    pub fn open() -> Result<Self, smol::io::Error> {
         let fd: RawFd = unsafe {
             libc::socket(
                 libc::AF_BLUETOOTH,
@@ -58,7 +57,7 @@ impl ManagementSocket {
         };
 
         if fd < 0 {
-            return Err(io::Error::last_os_error());
+            return Err(smol::io::Error::last_os_error());
         }
 
         let addr = SockAddrHci {
@@ -75,7 +74,7 @@ impl ManagementSocket {
             )
         } < 0
         {
-            let err = io::Error::last_os_error();
+            let err = std::io::Error::last_os_error();
 
             unsafe {
                 libc::close(fd);
@@ -84,7 +83,9 @@ impl ManagementSocket {
             return Err(err);
         }
 
-        let stream = unsafe { UnixStream::from_raw_fd(fd) };
+        let stream = unsafe { std::os::unix::net::UnixStream::from_raw_fd(fd) };
+        let stream = smol::Async::new(stream)?;
+        let stream = UnixStream::from(stream);
         let (read_stream, write_stream) = stream.split();
 
         Ok(ManagementSocket {
@@ -94,7 +95,7 @@ impl ManagementSocket {
     }
 
     /// Returns either an error or the number of bytes that were sent.
-    pub async fn send(&mut self, request: Request) -> Result<usize, io::Error> {
+    pub async fn send(&mut self, request: Request) -> Result<usize, smol::io::Error> {
         let buf: Bytes = request.into();
         self.writer.write(&buf).await
     }
